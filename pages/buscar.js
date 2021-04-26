ede8a611deae07876fdb12384eff283648629b1b
JTuyuc7 <50525507+JTuyuc7@users.noreply.github.com>
2021-04-26 13:17:27 -0600

    Finalizando buscador

--- pages/buscar.js
+++ pages/buscar.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../components/layout/Layout';
 import { useRouter } from 'next/router';
+import DetallesProducto from '../components/layout/DetallesProducto';
+import useProductos from '../hooks/useProductos';
+import styled from '@emotion/styled';
+
+const Listado = styled.div`
+    background-color: #f3f3f3;
+`;
+
+const Contenedor = styled.div`
+    max-width: 1200px;
+    width: 90%;
+    padding: 5rem 0;
+    margin: 0 auto;
+`;
+const BgWhite = styled.ul`
+    background-color: #fff;
+    padding: 3rem;
+`;
 
 const Buscar = () => {
 
@@ -8,12 +26,38 @@ const Buscar = () => {
     const router = useRouter();
     const { query : { q }} = router;
 
-    console.log(q);
+    const { productos } = useProductos('creado');
+
+    // State para almacenar el resultado de la busqueda
+    const [ resultado, guardarResultado ] = useState([]);
+
+    useEffect(() => {
+        const busqueda = q.toLowerCase();
+
+        const filtro = productos.filter( producto => {
+            return (
+                producto.nombre.toLowerCase().includes( busqueda ) || 
+                producto.descripcion.toLowerCase().includes( busqueda )
+            )
+        })
+        guardarResultado( filtro );
+    }, [ q, productos ])
 
     return (  
         <>
             <Layout>
-                <h1>Buscar El producto</h1>
+                <Listado>
+                    <Contenedor>
+                        <BgWhite>
+                        { resultado.map( producto => (
+                            <DetallesProducto
+                            key={ producto.id}
+                            producto={ producto }
+                            />
+                        ))}
+                        </BgWhite>
+                    </Contenedor>
+                </Listado>
             </Layout>
         </>
     );
